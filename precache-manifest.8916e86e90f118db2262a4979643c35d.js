e2fa50d39371f7e9b5c050581d67588dd8291795
mitrich83 <dmitry.v.malykhin@gmail.com>
2021-09-07 20:22:18 +0300

    Updates

---
+++ precache-manifest.8916e86e90f118db2262a4979643c35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345098e78b147698e76c273302fe9b63",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "b1328bd60298b775d383",
+    "url": "/Todolist/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "96d23b43498f2efdd3a7",
+    "url": "/Todolist/static/js/2.ff4f710e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist/static/js/2.ff4f710e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1328bd60298b775d383",
+    "url": "/Todolist/static/js/main.b3e2e00a.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
